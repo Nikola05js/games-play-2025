8f5b5a01c27937d5d8eb308d286aa307b97d7be9
Nikola05js <119896831+Nikola05js@users.noreply.github.com>
2025-04-20 19:51:47 +0300

    implement login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,25 +1,57 @@
+import { useNavigate } from "react-router-dom";
+import { useLogin } from "../../hooks/useAuth";
+import { useForm } from "../../hooks/useForm";
+
 export default function Login() {
-    return(
-       <>
-            {/* <!-- Login Page ( Only for Guest users ) --> */}
-        <section id="login-page" className="auth">
-            <form id="login">
+    const login = useLogin();
+    const navigate = useNavigate(); 
+    const { values, changeHandler, submitHandler } = useForm(
+        { email: "", password: "" },
+        async ({ email, password }) => {
+            try {
+                await login(email, password)
+                navigate('/')
+            } catch (error) {
+                consol.log(error.message)
+            }
+        }
+    );
 
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com"/>
+  return (
+    <>
+      {/* <!-- Login Page ( Only for Guest users ) --> */}
+      <section id="login-page" className="auth">
+        <form id="login" onSubmit={submitHandler}>
+          <div className="container">
+            <div className="brand-logo"></div>
+            <h1>Login</h1>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={values.email}
+              onChange={changeHandler}
+              placeholder="Sokka@gmail.com"
+            />
 
-                    <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password"/>
-                    <input type="submit" className="btn submit" value="Login"/>
-                    <p className="field">
-                        <span>If you don't have profile click <a href="#">here</a></span>
-                    </p>
-                </div>
-            </form>
-        </section>
-       </>
-    )
+            <label htmlFor="login-pass">Password:</label>
+            <input
+              type="password"
+              id="login-password"
+              name="password"
+              value={values.password}
+              onChange={changeHandler}
+            />
+            <input type="submit" className="btn submit" value="Login" />
+            <p className="field">
+              <span>
+                If you don't have profile click <a href="#">here</a>
+              </span>
+            </p>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
